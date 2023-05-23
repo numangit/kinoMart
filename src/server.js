b3488b9c9bc5613c7f3280f548d65fbc28658eed
numangit <abdulnumanjob@gmail.com>
2023-05-23 16:14:48 +0600

    home endpoint is created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");//get express function from express package
 const app = express();//assign express function to app
 
+//endpoints
+app.get("/", (req, res) => {
+  res.send("Welcome to KinoMart server..");
+});
+
 app.listen(3001, () => {
   console.log("KinoMart server is running at http://localhost:3001");
 });
