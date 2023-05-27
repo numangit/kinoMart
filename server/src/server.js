73c2166d14809a41dff858f81f539c82e6fedb48
numangit <abdulnumanjob@gmail.com>
2023-05-27 21:54:27 +0600

    hidden port set in env file

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@
 
 const app = require("./app");
 
-app.listen(3001, () => {
-  console.log("KinoMart server is running at http://localhost:3001");
+const port = process.env.SERVER_PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`KinoMart server is running at http://localhost:${port}`);
 });
