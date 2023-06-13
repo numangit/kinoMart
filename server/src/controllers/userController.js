454694c3cd4bcea81c90babce96df1591e802438
numangit <abdulnumanjob@gmail.com>
2023-06-13 20:24:05 +0600

    code added to exclude password field from users fetch results

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -9,6 +9,7 @@ const getUsers = async (req, res, next) => {
     const searchKeyword = req.query.search || "";
     const searchRegexp = new RegExp(".*" + searchKeyword + ".*", 'i'); //before or after search word doesn't matter and i is for case insensitive
 
+    //filtration and queries
     const filterQuery = {
       isAdmin: { $ne: true }, //not admin
       $or: [ //filter by searched name, email or phone
@@ -17,8 +18,9 @@ const getUsers = async (req, res, next) => {
         { phone: { $regex: searchRegexp } },
       ]
     };
+    const options = { password: 0 }; //to exclude the password field from results
 
-    const users = await User.find(filterQuery);
+    const users = await User.find(filterQuery, options);
     res.status(200).send({
       message: "Users data has been sent",
       users,
