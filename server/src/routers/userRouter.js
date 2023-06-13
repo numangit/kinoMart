61429f86e939e61f4bd560cdd068f8f026419c0f
numangit <abdulnumanjob@gmail.com>
2023-06-13 18:14:19 +0600

    seed user endpoint updated

--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -3,6 +3,6 @@ const { getUsers } = require("../controllers/userController");
 const userRouter = express.Router(); //getting the router function from express
 
 //users endpoint
-userRouter.get('/', getUsers);
+userRouter.get('/users', getUsers);
 
 module.exports = userRouter;
