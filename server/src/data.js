783c0eddf7ae521e35c55c9c6f5c43d240207085
numangit <abdulnumanjob@gmail.com>
2023-06-13 16:48:08 +0600

    testing data added

---
+++ server/src/data.js
@@ -0,0 +1,18 @@
+const data = {
+  users: [
+    {
+      name: 'Abdul Al Numan',
+      email: 'abcdhyahs@gmail.com',
+      password: '123456',
+      phone: '04055566',
+      address: 'Chittagong, Bangladesh',
+    },
+    {
+      name: 'David Warren',
+      email: 'david@gmail.com',
+      password: '123456',
+      phone: '04055566',
+      address: 'Dhaka, Bangladesh',
+    }
+  ]
+}
