3fc1c811b19bf176d4a3daf7bb101c0b8d83b299
numangit <abdulnumanjob@gmail.com>
2023-06-17 17:54:53 +0600

    error response added to the main server error handler

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const xssClean = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const userRouter = require('./routers/userRouter');
 const seedRouter = require('./routers/seedRouter');
+const { errorResponse } = require('./controllers/responseController');
 
 const app = express();//assign express function to app
 
@@ -49,8 +50,8 @@ app.use((req, res, next) => {
 
 //server error handling middleware -> all the endpoint errors will come here
 app.use((err, req, res, next) => {
-  return res.status(err.status || 500).json({
-    success: false,
+  return errorResponse(res, {
+    statusCode: err.status,
     message: err.message
   });
 });
