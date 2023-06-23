61e366a2751dc2c2acf61f0f91364ffaa75a64fe
numangit <abdulnumanjob@gmail.com>
2023-06-23 19:02:21 +0600

    delete user route added

--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -5,5 +5,5 @@ const userRouter = express.Router(); //getting the router function from express
 //users endpoint
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById);
-
+userRouter.delete('/:id', deleteUserById);
 module.exports = userRouter;
